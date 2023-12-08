d8cd2f743d2fe0c1685ab239a8d27663b9ca61f8
Marina Malak <marinamalak747@gmail.com>
2023-12-09 00:43:12 +0200

    first commit

---
+++ task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(void){
+	char c;
+	int x;
+	float y;
+	printf("Enter the char");
+	scanf("%c",&c);	
+	printf("Enter the int number");
+	scanf("%d",&x);	
+	printf("Enter the float number");
+	scanf("%f",&y);
+	printf("char = %c \n",c);
+	printf("int number = %d \n",x);
+	printf("float number = %f \n",y);
+	
+}
